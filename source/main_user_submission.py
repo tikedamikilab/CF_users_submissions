e4d2d3b3ce1cb8e50bb5b739ead17598b88c779a
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-08-16 11:49:41 +0900

    .gitignore

--- source/main_user_submission.py
+++ source/main_user_submission.py
@@ -1,8 +1,18 @@
-# 
-
+# main
+# チョットだけ楽にする
+from multiprocessing import Process
 import scraping_URL_user_submissons_problem
 import scraping_user_submission
 
 if __name__ == "__main__":
-    scraping_URL_user_submissons_problem.makeSubmissonsProblemURL('tourist', 1, 48)
-    scraping_user_submission.makeUserSubmissionsCsv('tourist', 1, 48)
+    # userName = 'Um_nik'
+    # start = 1
+    # end = 108
+    userName = 'tourist'
+    start = 1
+    end = 48
+    Process(
+        scraping_URL_user_submissons_problem.makeCSVSubmissonsProblemURL(userName, start, end),
+        scraping_user_submission.makeCSVUserSubmissions(userName, start, end)
+    )
+    
