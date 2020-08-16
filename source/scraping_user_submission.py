86207704c8d65cd94ef603514beeac921b1a613e
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-08-16 09:06:57 +0900

    user_submissionsできた

---
+++ source/scraping_user_submission.py
@@ -0,0 +1,27 @@
+# scraping_user_submission
+# CFからあるユーザのsubmissionsを取得 -> csvへ
+#
+
+import pandas as pd
+from datetime import datetime as dt
+import time
+from urllib import request  # urllib.requestモジュールをインポート
+from bs4 import BeautifulSoup  # BeautifulSoupクラスをインポート
+
+def makeUserSubmissionsCsv():
+    start = 1
+    end = 1
+    userName = 'tourist'
+    baseURL = 'https://codeforces.com/submissions/'
+    
+    for i in range(start , end + 1):
+        URL = baseURL + userName + '/page/' + str(i)
+        print(URL)
+
+        data = pd.read_html(URL, header = 0)
+        print(data)
+        data[5].to_csv("./users_submissions/test.csv", header=False, index=False, mode='a')
+
+if __name__ == "__main__":
+    makeUserSubmissionsCsv()
+    
