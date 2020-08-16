1f61a68d94f4e0a3b843ac87d2ee873c5d216ae7
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-08-16 09:18:33 +0900

    調整

--- source/scraping_user_submission.py
+++ source/scraping_user_submission.py
@@ -9,9 +9,10 @@ from urllib import request  # urllib.requestモジュールをインポート
 from bs4 import BeautifulSoup  # BeautifulSoupクラスをインポート
 
 def makeUserSubmissionsCsv():
-    start = 1
-    end = 1
     userName = 'tourist'
+    start = 1
+    end = 48
+    
     baseURL = 'https://codeforces.com/submissions/'
     
     for i in range(start , end + 1):
@@ -19,7 +20,6 @@ def makeUserSubmissionsCsv():
         print(URL)
 
         data = pd.read_html(URL, header = 0)
-        print(data)
         data[5].to_csv('./users_submissions/'+ userName +'.csv', header=False, index=False, mode='a')
 
 if __name__ == "__main__":
