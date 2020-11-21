497ef09d3d7a47e980cad836b4dc7376f5ac290f
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-11-22 01:38:40 +0900

    コメント追加

--- source/cos_rate_check.py
+++ source/cos_rate_check.py
@@ -2,6 +2,9 @@
 # 状況不明
 # おそらくcsvからcos類似度の結果を取得するはず
 # エラー内容 > AttributeError: 'Series' object has no attribute 'flatten'
+# flattenのコメントアウト
+# プログラムとしては，特定の問題IDを指定して，関係のある問題を抽出するもの
+
 
 import pandas as pd
 import csv
@@ -9,7 +12,7 @@ import csv
 def sample():
     baseDir = 'users_rate_change/0_cos_check3_0.2.csv'
     data = pd.read_csv(baseDir)
-    index = data['problemID'].flatten()
+    # index = data['problemID'].flatten()
     print(data['cos'][0][1:-1].split(','))
 
     solvedList = [71, 158, 118, 50, 231, 344, 268, 520, 25, 474, 671, 701, 615, 456, 624, 352, 707, 706, 742, 743, 752, 750, 744, 754, 758, 765, 767, 776, 777, 779, 782, 785, 791, 787, 792, 789, 801, 798, 805, 806,
