6e69f4c9a53062a8e79e2f1dfa2fd91690f9e195
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-11-22 01:42:34 +0900

    コメント追加

--- source/main_user_submission.py
+++ source/main_user_submission.py
@@ -1,5 +1,15 @@
 # main
-# チョットだけ楽にする
+# 並列実行を行うプログラム
+# 実行しているプログラムはProcess内を参照
+
+# 注意 > makeCSVらしい．
+
+# userName = ユーザのID
+# start=スクレイピングを始めるページの番号．通常１
+# end= スクレイピングの終わりのページ．その人個人のページからMAXのページ番号を取得
+# ユーザページ例
+# https://codeforces.com/submissions/tourist
+
 from multiprocessing import Process
 import scraping_URL_user_submissons_problem
 import scraping_user_submission
